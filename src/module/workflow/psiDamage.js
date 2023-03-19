8138ee9884ae1abdc96e1e4cb99e2b434b11e543
Yurii Denysenko <yuridevx@gmail.com>
2023-03-19 14:37:18 +0000

    initial

--- src/module/workflow/psiDamage.js
+++ src/module/workflow/psiDamage.js
@@ -1,14 +1,23 @@
+import {PsiActor} from "../data/PsiActor.mjs";
+
 export function psiDamage(item, rollConfig) {
+  if (item?.name === "Damage Over Time") {
+    return;
+  }
   if (!item.actor) return
   const parts = item?.system?.damage?.parts
   if (!parts) return
+  const psiFire = PsiActor.psiFire(item.actor)
+  const psiPsy = PsiActor.psiPsy(item.actor)
+  if (!psiFire && !psiPsy) return
+
   for (const part of parts) {
     const damageType = part[1]
-    if (damageType === "fire") {
-      rollConfig.parts.push(["@psiFire"])
+    if (Number.isFinite(psiFire) && damageType === "fire") {
+      rollConfig.parts.push(psiFire)
     }
-    if (damageType === "psychic") {
-      rollConfig.parts.push(["@psiPsy"])
+    if (Number.isFinite(psiPsy) && damageType === "psychic") {
+      rollConfig.parts.push(psiPsy)
     }
   }
 }
